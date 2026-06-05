d0a4a2379ff64a8a828b4ee4f308d0432c7521e7
김규태 <kimgyutea@naver.com>
2026-06-05 20:54:16 +0900

    feat: improve item color display

--- ui.hpp
+++ ui.hpp
@@ -19,14 +19,17 @@
 #define COLOR_PANEL_PAIR     12  // 패널 테두리
 #define COLOR_FEVER_PAIR     13  // 피버 아이템
 #define COLOR_DANGER_PAIR    14  // 조건부 게이트
+#define COLOR_GAMEOVER_PAIR  15  // 게임오버 팝업
+#define COLOR_GROWTH_TEXT_PAIR 16 // 미션보드 성장 기호
+#define COLOR_POISON_TEXT_PAIR 17 // 미션보드 독 기호
 
 //  맵 타일 출력 문자
 #define CHAR_WALL      "■ "
 #define CHAR_IWALL     "▩ "
 #define CHAR_HEAD      "● "
 #define CHAR_BODY      "○ "
-#define CHAR_GROWTH    "+ "
-#define CHAR_POISON    "- "
+#define CHAR_GROWTH    "＋"
+#define CHAR_POISON    "－"
 #define CHAR_GATE      "G "
 #define CHAR_FEVER     "F "
 #define CHAR_DANGER_GATE "! "
@@ -96,5 +99,7 @@ void cleanupUI();
 - 피버 아이템과 길이 5 조건부 게이트를 구분해서 표시하기 위한 색상 상수와 출력 문자 추가
 - 게임오버 화면에서 입력된 키를 main.cpp가 받을 수 있도록 showGameOver() 반환형을 int로 변경
 - 성장 아이템, 독 아이템, 일반 게이트, 조건부 게이트 기호를 +, -, G, !로 통일
+- 성장 아이템은 초록 배경/진한 노란색 ＋, 독 아이템은 빨간 배경/흰색 －, 조건부 게이트는 검은 배경/분홍 !로 표시하도록 변경
+- 미션보드 성장/독 기호는 배경 없이 초록 +, 빨간 -로 표시하도록 색상 상수 추가
 - 벽과 면역벽 문자도 2칸을 차지하도록 바꿔 이전 프레임 문자가 남는 잔상 문제 해결
 */
